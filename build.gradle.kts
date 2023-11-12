2077c5cbfc549879309c93236b63936ca5656493
Daniel <danielbergeron3@gmail.com>
2023-11-12 10:41:03 -0500

    Initial Commit Added dependencies, safe args and updated build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.4"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.2" apply false
 }
