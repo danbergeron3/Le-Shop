595cf7a139dbc27bad20f70ae5e00f5f526b05a1
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2023-11-18 12:28:27 -0500

    Initial Commit Created Constants class and example on how future Constants should be implemented.

---
+++ app/src/main/java/com/mobileapp/le_shop/Constants.java
@@ -0,0 +1,8 @@
+package com.mobileapp.le_shop;
+
+public class Constants {
+    // add Global Constants here
+    // Use following format:
+    // public static final String SHOP_NAME = "SAHARA";
+    // Try not to delete constants once they are added others could be using them
+}
