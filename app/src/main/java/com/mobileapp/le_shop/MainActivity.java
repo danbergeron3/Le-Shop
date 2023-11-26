58472a0339969355f5b09656aa54828dbd0f8086
NishanthPotturu <nishanth_potturu@yahoo.com>
2023-11-26 15:46:52 -0500

    first fragment push

--- app/src/main/java/com/mobileapp/le_shop/MainActivity.java
+++ app/src/main/java/com/mobileapp/le_shop/MainActivity.java
@@ -1,8 +1,19 @@
 package com.mobileapp.le_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(navView, navController);
+        AppBarConfiguration.Builder builder = new AppBarConfiguration.Builder(navController.getGraph());
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        builder.setOpenableLayout(drawerLayout);
+        AppBarConfiguration appBarConfiguration = builder.build();
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        NavController navController =
+                Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.onNavDestinationSelected(item,
+                navController) || super.onOptionsItemSelected(item);
     }
 }
