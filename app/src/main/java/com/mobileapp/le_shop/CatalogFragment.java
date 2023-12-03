6bdd10ccd6509443f1a7a5ac03b7efa444e0947e
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2023-12-03 14:20:04 -0500

    Added string resources, created textviews and spinners items in CatalogFragment. Wrote populate functions for fillter, conected them with db.

--- app/src/main/java/com/mobileapp/le_shop/CatalogFragment.java
+++ app/src/main/java/com/mobileapp/le_shop/CatalogFragment.java
@@ -1,17 +1,16 @@
 package com.mobileapp.le_shop;
 
-import static com.mobileapp.le_shop.Populate.populateView;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.mobileapp.le_shop.databinding.FragmentCatalogBinding;
 
@@ -28,11 +27,60 @@ public class CatalogFragment extends Fragment {
         @NonNull FragmentCatalogBinding binding
                 = FragmentCatalogBinding.inflate(inflater,container, false);
         View view = binding.getRoot();
-        try {
-            Populate.populateView(binding.linearLayout1, binding.getRoot().getContext());
-        } catch (IOException e) {
-            Log.e("DB_ADAPTER", "Failed to create database");
-        }
+
+        // filter system
+        // add spinner adapter
+        Spinner spinnerItemType = binding.itemSpinner;
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                binding.getRoot().getContext(),
+                R.array.item_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        spinnerItemType.setAdapter(adapter);
+
+        spinnerItemType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                // Perform actions when an item is selected
+                String selectedItemType = parentView.getItemAtPosition(position).toString();
+                switch (selectedItemType) {
+                    case "Shirts":
+                        try {
+                            binding.linearLayout1.removeAllViews();
+                            Populate.populateViewWithShirts(binding.linearLayout1, binding.getRoot().getContext());
+                        } catch (IOException e) {
+                            Log.e("DB_ADAPTER", "Failed to create database");
+                        }
+                        return;
+                    case "Pants":
+                        try {
+                            binding.linearLayout1.removeAllViews();
+                            Populate.populateViewWithPants(binding.linearLayout1, binding.getRoot().getContext());
+                        } catch (IOException e) {
+                            Log.e("DB_ADAPTER", "Failed to create database");
+                        }
+                        return;
+                    default:
+                        try {
+                            binding.linearLayout1.removeAllViews();
+                            Populate.populateViewAll(binding.linearLayout1, binding.getRoot().getContext());
+                        } catch (IOException e) {
+                            Log.e("DB_ADAPTER", "Failed to create database");
+                        }
+                        return;
+                }
+            }
+            // required but not needed
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {}
+        });
+
+
+
 
        return view;
     }
