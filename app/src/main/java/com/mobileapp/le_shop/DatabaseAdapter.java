6d999c5e1afcae95201ad2d4c0ea59076688e6b2
rjmara <ryanjosue.rj@gmail.com>
2023-12-03 20:07:26 -0500

    completed function stubs :D

--- app/src/main/java/com/mobileapp/le_shop/DatabaseAdapter.java
+++ app/src/main/java/com/mobileapp/le_shop/DatabaseAdapter.java
@@ -100,16 +100,42 @@ public class DatabaseAdapter {
 
     }
 
+    /**
+     * Returns all Shop Items from the Items table that
+     * are shirts.
+     * Returns null if no results.
+     * @return ArrayList of ShopItem
+     */
     public ArrayList<ShopItem> getAllShirts() {
         //TODO: Similar to getAllPants
-        return null;
+        ArrayList<ShopItem> list = new ArrayList<ShopItem>();
+        String sql = "Select * from Items Natural Join Shirts";
+        /* COLUMNS: item_id name description image price size */
+        Cursor cr = db.rawQuery(sql, null);
+
+        if (cr.getCount() == 0) {
+            return null;
+        }
+
+        for (int i = 0; i < cr.getCount(); i++) {
+            cr.moveToPosition(i);
+            int item_id = cr.getInt(0);
+            String name = cr.getString(1);
+            String desc = cr.getString(2);
+            float price = cr.getFloat(4);
+            String size = cr.getString(5);
+
+            ShopItem item = new ShopItem(item_id, name, desc, price, size);
+            list.add(item);
+        }
+        return list;
     }
 
     /**
      * Returns all Shop Items from the Items table, does not
      * return distinct sizes and size field will be NULL.
      * Returns null if no results.
-     * @return ArraList of ShopItem
+     * @return ArrayList of ShopItem
      */
     public ArrayList<ShopItem> getAllShopItems() {
         ArrayList<ShopItem> list = new ArrayList<ShopItem>();
@@ -164,24 +190,149 @@ public class DatabaseAdapter {
         return item;
     }
 
+    /**
+     * Returns Shop Items from the Items table with the given id.
+     * Returns all sizes of the shop item id.
+     * Returns null if no results.
+     * @param id
+     * @return
+     */
     public ArrayList<ShopItem> getShopItemAndSizesFromId(int id) {
         // TODO: Return shop item with same id and all of its sizes
-        return null;
+        ArrayList<ShopItem> list = new ArrayList<ShopItem>();
+        String sql = String.format("select * from Items natural join Shirts natural join Pants where item_id = %d", id);
+        Cursor cr = db.rawQuery(sql, null);
+
+        // Check if Empty
+        if(cr.getCount() == 0) {
+            return null;
+        }
+
+        // Comb the list and make the ShopItems
+        for (int i = 0; i < cr.getCount(); i++) {
+            cr.moveToPosition(i);
+            int item_id = cr.getInt(0);
+            String name = cr.getString(1);
+            String desc = cr.getString(2);
+            float price = cr.getFloat(4);
+            String size = cr.getString(5);
+
+            ShopItem item = new ShopItem(item_id, name, desc, price, size);
+            list.add(item);
+        }
+        return list;
     }
 
+    /**
+     * Returns ArrayList of sizes of a given id
+     * Returns null if no results.
+     * @param id
+     * @return
+     */
     public ArrayList<String> getAllSizesFromId(int id) {
         // TODO: Natural Join Shirts and Pants and match the id, get all sizes
-        return null;
+        ArrayList<String> list = new ArrayList<String>();
+        String sql = String.format("select * from Items natural join Shirts natural join Pants where item_id = %d", id);
+        Cursor cr = db.rawQuery(sql, null);
+
+        // Check if Empty
+        if(cr.getCount() == 0) {
+            return null;
+        }
+
+        for (int i = 0; i < cr.getCount(); i++) {
+            cr.moveToPosition(i);
+            String size = cr.getString(5);
+            list.add(size);
+        }
+
+        return list;
     }
 
+    /**
+     * Returns all Shop Items from the Featured Items table, does not
+     * return distinct sizes and size field will be NULL.
+     * Returns null if no results.
+     * @return ArrayList of ShopItem
+     */
     public ArrayList<ShopItem> getAllFeaturedItems() {
         // TODO: Return all the items from the Featured_Items table, will have a null size
-        return null;
+        ArrayList<ShopItem> list = new ArrayList<ShopItem>();
+        String sql = String.format("select * from Featured_Items natural join Items");
+        Cursor cr = db.rawQuery(sql, null);
+
+        // Check if Empty
+        if(cr.getCount() == 0) {
+            return null;
+        }
+
+        // Comb the list and make the ShopItems
+        for (int i = 0; i < cr.getCount(); i++) {
+            cr.moveToPosition(i);
+            int item_id = cr.getInt(0);
+            String name = cr.getString(1);
+            String desc = cr.getString(2);
+            float price = cr.getFloat(4);
+            String size = null;
+
+            ShopItem item = new ShopItem(item_id, name, desc, price, size);
+            list.add(item);
+        }
+        return list;
     }
 
-    public void getAllCartItems() {
-        // TODO: Return all of the items in the Cart_Items table
-        // TODO: Want to make a Cart Item class to store quantity
+    /**
+     * Returns all Shop Items from the Cart Items table, does not
+     * return distinct sizes and size field will be NULL.
+     * Quantity is not included; there is a quantity adapter function.
+     * these items hold TOTAL price (price * quantity).
+     * Returns null if no results.
+     * @return ArrayList of ShopItem
+     */
+    public ArrayList<ShopItem> getAllCartItems() {
+        ArrayList<ShopItem> list = new ArrayList<ShopItem>();
+        String sql = "Select * from Cart_Items";
+        Cursor cr = db.rawQuery(sql, null);
+
+        // Check if Empty
+        if(cr.getCount() == 0) {
+            return null;
+        }
+
+        // Comb the list and make the ShopItems
+        for (int i = 0; i < cr.getCount(); i++) {
+            cr.moveToPosition(i);
+            int item_id = cr.getInt(0);
+            String name = cr.getString(4);
+            String desc = cr.getString(5);
+            float price = cr.getFloat(7);
+            String size = cr.getString(1);
+
+            ShopItem item = new ShopItem(item_id, name, desc, price, size);
+            list.add(item);
+        }
+        return list;
+    }
+
+    /**
+     * Returns quantity of shop item from Cart Item table.
+     * Returns -1 if no results.
+     * @param id
+     * @param size
+     * @return quantity
+     */
+    public int getCartItemQuantity(int id, String size) {
+        String sql = String.format("select * from Cart_Items in (%d) and size = '%s'", id, size);
+        Cursor cr = db.rawQuery(sql, null);
+
+        if(cr.getCount() == 0) {
+            return -1;
+        }
+
+        cr.moveToFirst();
+        int quantity = cr.getInt(2);
+
+        return quantity;
     }
 
     /*
@@ -189,13 +340,49 @@ public class DatabaseAdapter {
         NOTE: These are not as important since the Cart is our extra, please focus on the above
      */
 
+    /**
+     * Add item to the Cart Item table
+     * @param item
+     */
     public void addCartItem(ShopItem item) {
         // TODO: Add the item to the cart, size must NOT BE NULL
+        String sql = String.format("select * from Cart_Items in (%d) and size = '%s'", item.getId(), item.getSize());
+        Cursor cr = db.rawQuery(sql, null);
+        String modify_sql;
+
+        // Check if Empty
+        if(cr.getCount() == 0) {
+            modify_sql = String.format("insert into Cart_Items(" + item.getId() + item.getSize() + 1 + item.getPrice() + ")");
+        } else {
+            int quantity = getCartItemQuantity(item.getId(), item.getSize());
+            modify_sql = String.format("update Cart_Items set quantity = %d where id = %d", quantity, item.getId());
+        }
+        db.execSQL(modify_sql);
     }
 
+    /**
+     * Removes an item from the cart
+     * @param id
+     * @param size
+     */
     public void removeCartItem(int id, String size) {
         // TODO: Remove item from the cart with this id and size
         // TODO: Might need more logic to reduce quantity and such
+        String sql = String.format("select * from Cart_Items in (%d) and size = '%s'", id, size);
+        Cursor cr = db.rawQuery(sql, null);
+        String modify_sql;
+
+        // Check if there is at least one item
+        if(cr.getCount() != 0) {
+            int quantity = getCartItemQuantity(id, size);
+            if (quantity == 1) {
+                modify_sql = String.format("delete from Cart_Items where id = %d", id);
+            } else {
+                modify_sql = String.format("update Cart_Items set quantity = %d where id = %d", quantity - 1, id);
+            }
+            db.execSQL(modify_sql);
+        }
+
     }
 
 }
